ee3b9b6019e606392cee7efc25931ebe67eac1eb
Sebastian Zielke <harry48@arcor.de>
2026-03-22 21:14:16 +0100

    converrted Avatar to funcional component

--- src/Avatar.tsx
+++ src/Avatar.tsx
@@ -1,10 +1,13 @@
-import { Component, type ReactNode } from "react";
 import { createAvatar } from '@dicebear/core';
 import { lorelei } from '@dicebear/collection';
 
 interface ButtonProps {
     onClick: () => void;
     svg: string; // Prop for the SVG code
+}
+
+interface AvatarProps {
+    seed: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ onClick, svg }) => (
@@ -14,28 +17,23 @@ const Button: React.FC<ButtonProps> = ({ onClick, svg }) => (
 );
 
 
-class Avatar extends Component {
-
-    render(): ReactNode {
-        const svgString = "<svg width='50' height='50'><circle cx='10' cy='10' r='5' stroke='black' strokeWidth='3' fill='red' /></svg>";
-        return <>
-            <Button
-                onClick={() => console.log('Svg')}
-                svg={svgString}
-            />
-            <div>
-                <img src={this.generateNewAvatar().toDataUri()} alt="Avatar" />
-            </div>
-        </>
-    }
-
-
-    generateNewAvatar() {
+const Avatar: React.FC<AvatarProps> = ({ seed }) => {
+    const generateNewAvatar = (seed: string) => {
         const avatar = createAvatar(lorelei, {
-            seed: 'Friday',
+            seed: seed,
         });
         return avatar;
     }
+    const svgString = "<svg width='50' height='50'><circle cx='10' cy='10' r='5' stroke='black' strokeWidth='3' fill='red' /></svg>";
+    return <>
+        <Button
+            onClick={() => console.log('Svg')}
+            svg={svgString}
+        />
+        <div>
+            <img src={generateNewAvatar(seed).toDataUri()} alt="Avatar" />
+        </div>
+    </>
 }
 
 export default Avatar
